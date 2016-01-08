c511cb624d7187268af9cbaed56bc6a221a4eaf6
massoudTheDude <mahermassoud@gmail.com>
2016-01-08 13:51:32 -0800

    Read in file, added notebook for dev testing purposes

--- data.py
+++ data.py
@@ -1,10 +1,20 @@
 # Class for fetching data from matrix
 # Author: Massoud Maher
 
-import csv
+import pandas as pd
+import os
 
 class CellGene:
 
   # "Contructor" reads cell-gene matrix into memory
   def __init__(self):
-    cell_gene =     
+    self.cell_matrix = pd.read_csv('CCLE_inferred_prot_abundance.tab', sep='\t', index_col="Description")
+    
+
+  # function to test this class during development
+  def test(self):
+    print self.cell_matrix["X769P_KIDNEY"]
+
+# "main"
+cg = CellGene()
+cg.test()
