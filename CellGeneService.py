2ea4aff5820533588aa6338021031658c6187e64
massoudTheDude <mahermassoud@gmail.com>
2016-01-19 10:53:28 -0800

    Begin taking JSON input, finalized JSON format

--- CellGeneService.py
+++ CellGeneService.py
@@ -4,8 +4,50 @@ import json
 from CellGene import CellGene
 from bottle import route, run, template, request
 
+# TODO put json format in README
 class CellGeneService:
   """Runs REST service that fetches abundance values for cell-gene pair"""
+
+
+  @route('/json_test', method='POST')
+  def test_json():
+    """Returns a JSON of abundance values for the provided JSON inputs
+
+    Returns:
+      A JSON string representing the associated abundance values of cell_line and gene in the following format
+
+      {
+        "gene_name":
+          {
+            "cell_line_name":abundance_val
+            ...
+          },
+
+        ...
+
+        "gene_name":
+          {
+            "cell_line_name":abundance_val
+            ...
+          }
+        }
+
+      Returns Null if input is invalid (including integers) or not found in matrix
+    """
+    print("JSON is: " + str(request.json))
+    print("Type is: " + str(type(request.json)))
+    print("values are: " + str( request.json.values() ))
+    print("keys are: " + str( request.json.keys() ))
+
+    # Dictionary of gene:cell pairs
+    input_set = request.json
+
+
+    # For each gene
+    for(key in input_set.keys()):
+      abundance_list = get
+
+
 
 
   @route('/context/expression/cell_line/gene_set=<gene_set>cell_line_set=<cell_line_set>', method='GET')
@@ -52,5 +94,5 @@ class CellGeneService:
     """Runs the service on localhost:8080"""
     run(host='localhost', port=8080, debug=True)
 
-if __name__ == "__main__":
-  main()
+  if __name__ == "__main__":
+    main()
