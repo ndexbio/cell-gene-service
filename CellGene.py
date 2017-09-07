a2d7fc4c271d8cd750cab6171e3b0e69235f4c2f
Aaron Gary <agary@ucsd.edu>
2017-09-07 13:57:51 -0700

    Added logging.

--- CellGene.py
+++ CellGene.py
@@ -16,7 +16,6 @@ class CellGene(object):
 
   """
 
-
   def __init__(self, dataset):
     """Reads cell-gene matrix into memory
 
@@ -29,7 +28,6 @@ class CellGene(object):
 
     filename = dataset_dict[dataset]
     self.cell_matrix = pd.read_csv('CCLE_inferred_prot_abundance.tab', sep='\t', index_col="Description")
-    
 
   def test(self):
     """Function to test this class during development"""
@@ -73,6 +71,7 @@ class CellGene(object):
       Value will be None if no items found in dataset
     """
     # Check if inputs are contained in matrix, remove them if they're not
+    count = 0
     if type(cell_line) is set:
       new_cell_line = []
       for item in cell_line:
@@ -82,13 +81,21 @@ class CellGene(object):
           print item + " not found in matrix, skipping"
           continue
         else:
+          if count % 5000 == 0:
+            print "Appending item %s" % str(item)
+          if count > 5000000:
+            print 'Reached max item count'
+            break
+            #raise Exception('Reached max item count')
           new_cell_line.append(item)
+          count += 1
         cell_line = new_cell_line
     elif not (cell_line in self.cell_matrix.columns):
       print "*************************************"
       print cell_line + " not found in matrix, data lookup failed"
       cell_line = None
 
+    count = 0
     if type(gene) is set:
       new_gene = []
       for item in gene:
@@ -96,6 +103,11 @@ class CellGene(object):
           print "*************************************"
           print item + " not found in matrix, skipping"
         else:
+          if count % 5000 == 0:
+            print "Appending item %s" % str(item)
+          if count > 5000000:
+            print 'Reached max item count'
+            break
           new_gene.append(item)
         gene = new_gene
     elif not (gene in self.cell_matrix.index):
